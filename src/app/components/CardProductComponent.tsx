4d624a6e804500901d3fc411cf122fd11c022c95
Elvio <elvio.galeano@gmail.com>
2024-01-15 15:29:36 -0300

    fix

--- src/app/components/CardProductComponent.tsx
+++ src/app/components/CardProductComponent.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function CardProductComponent({ product }: any) {
   return (
-    <div className="flex flex-col  items-center justify-between bg-red-950 rounded-xl w-72 hover:scale-105 duration-300">
+    <div className="flex flex-col items-center justify-between bg-red-950 rounded-xl w-72 hover:scale-105 duration-300">
       <span className="py-3 font-bold">{product.name.toUpperCase()}</span>
       <Image
         className="h-72 object-cover"
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,6 +5,10 @@ export default function Footer() {
     <footer className="flex w-full py-10">
       <div className="text-center flex gap-4 justify-around flex-wrap w-full">
         <p>© 2024 ATR.Todos los derechos reservados.</p>
+        <div className="text-left">
+          <p>📅 De Lunes a Domingo de 20hs a 23hs</p>
+          <p>📍 Margarita Weild 3913 - Monte Chingolo</p>
+        </div>
         <p>
           Creado por{" "}
           <a
